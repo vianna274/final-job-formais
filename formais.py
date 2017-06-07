3047769952e4ac5f676e4940f2217fec31996a3a
Andy Ruiz Garramones <andyfutbolclip@yahoo.es>
2017-06-07 09:29:21 -0300

    Set mainVar

--- formais.py
+++ formais.py
@@ -1,8 +1,22 @@
 class Variable:
+        
+	mainVar = None
+	varsTerms = None
+	totalmenteVisitado = None
+	estado = None
     # mainVar = variavel da esquerda
     # varsTerms = lista de tuplas [("V", "var"),("*", "ponto") ("R","var"), ("oi","term")]
+    
     def __init__(mainVar, varsTerms, estado):
-        self.mainVar = mainVar
-        self.varsTerms = varsTerms
-        self.totalmenteVisitado = False
-        self.estado = estado
+            
+            self.mainVar = mainVar
+            self.varsTerms = varsTerms
+            self.totalmenteVisitado = False
+            self.estado = estado
+
+            
+          
+    def setmainVar(self,mainVar):
+            self.mainVar = mainVar
+    
+        
