8aeaa26ae6aae33e064d98e599281a34dceb50ac
Leonardo Vianna <leonardo.vianna.f@gmail.com>
2017-06-07 00:34:02 -0300

    [Class] First Class

---
+++ formais.py
@@ -0,0 +1,8 @@
+class Variable:
+    # mainVar = variavels da esquerda
+    # varsTerms = lista de tuplas [("V", "var"),("*", "ponto") ("R","var"), ("oi","term")]
+    def __init__(mainVar, varsTerms, estado):
+        self.mainVar = mainVar
+        self.varsTerms = varsTerms
+        self.totalmenteVisitado = False
+        self.estado = estado
