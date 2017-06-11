3070056680b2c9203e30c323b43aa3431c959705
Leonardo Vianna <leonardo.vianna.f@gmail.com>
2017-06-11 20:13:11 -0300

    [Reader] First Attempt

--- formais.py
+++ formais.py
@@ -3,7 +3,7 @@ from random import randint
 
 class GeneralClass():
     value = None
-    varsTerms = None
+    varsTerms = []
     totalmenteVisitado = None
     estado = None
 
@@ -12,7 +12,7 @@ class GeneralClass():
     # varsTerms = lista de tuplas [(V, "var"),("*", "ponto") (R,"var"), ("oi","term")]
     def __init__(self, value, varsTerms, estado):
         self.setValue(value)
-        self.setVarsTerms(varsTerms)
+        self.varsTerms = []
         self.setTotalmenteVisitado(False)
         self.setEstado(estado)
 
@@ -22,8 +22,8 @@ class GeneralClass():
     def getValue(self):
         return self.value
 
-    def setVarsTerms(self,varsTerms):
-        self.varsTerms = varsTerms
+    def setVarsTerms(self,terms):
+        self.varsTerms.append(terms)
 
     def getVarsTerms(self):
         return self.varsTerms
@@ -41,8 +41,12 @@ class GeneralClass():
         return self.estado
 
 class Variable(GeneralClass):
-    def __init__(self,mainVar, varsTerms, estado):
-        GeneralClass.__init__(self,mainVar, varsTerms, estado)
+    def __init__(self,value, varsTerms, estado):
+        GeneralClass.__init__(self,value, varsTerms, estado)
+
+    def __str__(self):
+        return "Variable " + str(self.value)
+
     def getRandomThing(self):
         lenghtTerms = len(self.varsTerms)
         randomTerm = self.varsTerms[randint(0,lenghtTerms-1)]
@@ -60,6 +64,9 @@ class Terminal():
         self.setValue(value)
         self.setGenre(genre)
         self.setNumber(number)
+
+    def __str__(self):
+        return "Terminal " + str(self.value)
 
     def setValue(self, value):
         self.value = value
@@ -89,8 +96,75 @@ def looping(P):
         terms = P.getRandomThing()
         for term in terms:
             looping(term)
+
+def readerFile():
+    terminals = []
+    variables = []
+    firstSymbol = ""
+    file_ = open("grammar.txt","r")
+    rows = file_.read().split('\n')
+    readingWhat = "Nothing"
+    for row in rows:
+        if row[0] == "[":
+            if readingWhat == "Terminais":
+                tempRow = row.split(' ')
+                terminals.append(Terminal(tempRow[1],"Masc","Sing"))
+            elif readingWhat == "Variaveis":
+                tempRow = row.split(' ')
+                variables.append(Variable(tempRow[1],[],"None"))
+            elif readingWhat == "Inicial":
+                tempRow = row.split(' ')
+                firstSymbol = tempRow[1]
+            elif readingWhat == "Regras":
+                row = row.replace(' ','')
+                row = row.replace('[',' ')
+                row = row.replace(']',' ')
+                row = row.replace('>', '')
+                tempRow = row.split(' ')
+                index = 0
+                for variable in variables:
+                    if tempRow[1] ==variable.getValue():
+                        print(variable)
+                        print(variable.getVarsTerms())
+                        queijo = alocateTerms(variables, terminals, tempRow[2:], variable)
+                        variable.varsTerms.append(queijo)
+                        print(variable.getVarsTerms())
+                        print("---")
+                    index += 1
+        else:
+            if row == "Terminais":
+                readingWhat = "Terminais"
+            elif row == "Variaveis":
+                readingWhat = "Variaveis"
+            elif row == "Inicial":
+                readingWhat = "Inicial"
+            elif row == "Regras":
+                readingWhat =  "Regras"
+
+def printVariables(variables):
+    for variable in variables:
+        print(variable)
+        print(variable.getVarsTerms())
+        #for term in variable.getVarsTerms():
+            #print(term)
+        print("-------------")
+
+def alocateTerms(variables, terminals, terms, mainVar):
+    tempTerms = []
+    for term in terms:
+        for variable in variables:
+            if term == variable.getValue():
+                tempTerms.append(variable.getValue())
+        for terminal in terminals:
+            if term == terminal.getValue():
+                tempTerms.append(terminal.getValue())
+    #print(tempTerms)
+    return tempTerms
+
 if __name__ == '__main__':
     # Ex: Linguagem
+
+
 
     V = Variable("V", [[Terminal("dos tempos", "Plural", "Masculino")], [Terminal("das cidades", "Plural", "Feminino")], [Terminal("das vidas", "Plural", "Masculino")]], "Sei La")
     R = Variable("R", [[Terminal("os homens", "Plural", "Masculino")],[Terminal("as mulheres", "Plural", "Feminino")]], "Sei La")
@@ -98,4 +172,4 @@ if __name__ == '__main__':
     Y = Variable("Y", [[Terminal("O inicio", "Singular", "Neutro")]], "Sei La")
     P = Variable("P",[[Y, V, R, J,], [Terminal("Ops hehe", "Singular", "Neutro")]], "Inicio")
 
-    looping(P)
+    readerFile()
