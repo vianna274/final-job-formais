65c95f971916d1108a1794f7ef6746c6bf7e62fc
Andy Ruiz Garramones <andyfutbolclip@yahoo.es>
2017-06-12 02:37:09 -0300

    Input de .txt para objetos [FALTA GERADOR CORRETO]

--- formais.py
+++ formais.py
@@ -1,14 +1,10 @@
-#user.py
+import re
+import copy
 from random import randint
 
-class GeneralClass():
-    value = None
-    varsTerms = []
-
-    # mainVar = variavel da esquerda
-    # varsTerms = terminais e variaveis do lado DIREITO
-    def __init__(self, value, varsTerms):
-        self.setValue(value)
+class Variable():
+    def __init__(self, valor, termos):
+        self.setValue(valor)
         self.varsTerms = []
 
     def setValue(self, value):
@@ -17,22 +13,16 @@ class GeneralClass():
     def getValue(self):
         return self.value
 
-    def setVarsTerms(self,terms):
-        self.varsTerms.append(terms)
+    def appendTerm(self,termos):
+        self.varsTerms.append(termos)
+
+    def expendTerm(self,termos):
+        self.varsTerms.extend(termos)
 
     def getVarsTerms(self):
         return self.varsTerms
 
-class Variable(GeneralClass):
-    # Herda GeneralClass (recebe todos os metodos e variaveis)
-    def __init__(self,value, varsTerms):
-        GeneralClass.__init__(self,value, varsTerms)
-
-    def __str__(self):
-        return "Variable " + str(self.value)
-
-    # Retorna um elemento aleatório do "varTerms"
-    def getRandomThing(self):
+    def getRandomThing(self): # Retorna um elemento aleatório do "varTerms"
         lenghtTerms = len(self.varsTerms)
         randomTerm = self.varsTerms[randint(0,lenghtTerms-1)]
         return randomTerm
@@ -40,11 +30,10 @@ class Variable(GeneralClass):
     def getClass(self):
         return "Variable"
 
-class Terminal():
-    genre = None
-    number = None
-    value = None
+    def __str__(self):
+        return "Variable " + str(self.value)
 
+class Terminal():
     def __init__(self, value, genre, number):
         self.setValue(value)
         self.setGenre(genre)
@@ -74,96 +63,104 @@ class Terminal():
     def getClass(self):
         return "Terminal"
 
-def firstLooping(firstSymbol, variables):
+def isTerminal(palavra, lista):
+    retorno = False
+    for i in lista:
+        if i.getValue() ==  palavra:
+            retorno = True
+    return retorno
+
+def printaVariaveis(variaveis):
+    for variavel in variaveis:
+        print(variavel,end=':\n')
+        for sublista in variavel.getVarsTerms():
+            for elemento in sublista:
+                print(elemento, end=' ')
+            print()
+        print('')
+
+#def firstLooping(firstSymbol, variables):
     # Vai procurar o SimboloInicial em todas as variaveis e utilizar ela
     # para começar a criar a gramatica
-    for variable in variables:
-        if variable.getValue() == firstSymbol:
+#    for variable in variables:
+#        if variable.getValue() == firstSymbol:
             # Se for uma loucura a gramatica começar com um terminal, já printa ele
-            if(variable.getClass() == "Terminal"):
-                print(variable.getValue())
+#            if(variable.getClass() == "Terminal"):
+#                print(variable.getValue())
             # Se for uma Variavel, vai pegar um elemento randomico do seu lado esquerdo
             # E executar do mesmo jeito esse termo
-            if (variable.getClass() == "Variable"):
-                terms = variable.getRandomThing()
-                for term in terms:
-                    looping(term)
+#            if (variable.getClass() == "Variable"):
+#                terms = variable.getRandomThing()
+#                for term in terms:
+#                    looping(term)
 
-def looping(variable):
+#def looping(variable):
     # Faz tudo igual a firstLooping, exceto a parte do firstSymbol
-    if(variable.getClass() == "Terminal"):
-        print(variable.getValue())
-    if (variable.getClass() == "Variable"):
-        terms = variable.getRandomThing()
-        for term in terms:
-            looping(term)
-
-def readerFile():
-    terminals = []
-    variables = []
-    firstSymbol = ""
-    file_ = open("grammar.txt","r")
-    rows = file_.read().split('\n')
-    readingWhat = "Nothing"
-    for row in rows:
-        # Se a linha começar com [ eu já sei que é o inicio de uma Variavel ou Terminal
-        if row[0] == "[":
-            # Se estiver na parte de Terminais, Variaveis ou Inicial seta para as 2 listas e a variavel.
-            if readingWhat == "Terminais":
-                tempRow = row.split(' ')
-                terminals.append(Terminal(tempRow[1],"Masc","Sing"))
-            elif readingWhat == "Variaveis":
-                tempRow = row.split(' ')
-                variables.append(Variable(tempRow[1],[]))
-            elif readingWhat == "Inicial":
-                tempRow = row.split(' ')
-                firstSymbol = tempRow[1]
-            # Se estiver na aba Regras, modifica a entrada para facilitar a leitura
-            elif readingWhat == "Regras":
-                row = row.replace(' ','')
-                row = row.replace('[',' ')
-                row = row.replace(']',' ')
-                row = row.replace('>', '')
-                tempRow = row.split(' ')
-                index = 0
-                for variable in variables:
-                    # O index é [1] porque o array é ["", "S", ">", "NP"]
-                    # Quando achar a variavel que inicia a regra, aloca as outras variaveis, terminais nela
-                    if tempRow[1] ==variable.getValue():
-                        queijo = alocateTerms(variables, terminals, tempRow[2:], variable)
-                        variable.varsTerms.append(queijo)
-        else:
-            # Le o arquivo e modifica a ação do loop anterior
-            if row == "Terminais":
-                readingWhat = "Terminais"
-            elif row == "Variaveis":
-                readingWhat = "Variaveis"
-            elif row == "Inicial":
-                readingWhat = "Inicial"
-            elif row == "Regras":
-                readingWhat =  "Regras"
-    return variables, terminals, firstSymbol
+#    if(variable.getClass() == "Terminal"):
+#        print(variable.getValue())
+#    if (variable.getClass() == "Variable"):
+#        terms = variable.getRandomThing()
+#        for term in terms:
+#            looping(term)
 
 
-def alocateTerms(variables, terminals, terms, mainVar):
-    # Vai ler cada termo da regra dada, ex: "[ S ] > [ NP ] [ VP ] ;1",
-    # aqui só chega a partir do: ""> [ NP ] [ VP ] ;1"
-    # (corto o símbolo inical que é a Variavel que já está sendo alocada)
-    # Eu vejo o "nome" do termo (var, ou terminal) e procura esse nome nas listas de: variables, terminals
-    # quando eu acho, eu do append nas varsTerms da variavel principal da regra
-    # terminando desse jeito: S(mainVar), varsTerm(do S) = [classNP,classVP]
-    tempTerms = []
-    for term in terms:
-        for variable in variables:
-            if term == variable.getValue():
-                tempTerms.append(variable)
-        for terminal in terminals:
-            if term == terminal.getValue():
-                tempTerms.append(terminal)
-    #print(tempTerms)
-    return tempTerms
+
+def readInput():
+    try: #Se testa abrir o arquivo sample.txt
+        arquivo = open('sample.txt','r')
+        palavras = []
+        variaveis = []
+        terminais = []  #Declaração de todas as variaveis a serem utilizadas
+        varsTerms = []
+        new_phrase = []
+        case = 0
+        startingWord = None
+
+        for line in arquivo: # A primeira linha contendo trash se ignora usando case 0 para quando ainda não se está lendo linhas corretas
+            if case == 0:
+                case += 1
+
+            elif '[ ' and ' ]' in line: #Se tiver palavras válidas na linha ( dentro de [ ] )
+                del new_phrase[:]
+                new_phrase = re.findall(r'\[ ([^]]*)\ ]', line) #salva a ou as palavras da linha na lista de novas palavras
+
+                if case == 4:   #Se estiver lendo as regras, tem mais de 1 palavra na linha
+                    del varsTerms[:]
+                    for variavelAtual in variaveis: #Procura a variavel da esquerda da regra na lsita de variaveis
+                            if variavelAtual.getValue() == new_phrase[0]:
+                                    for elemento in new_phrase[1:] :  #Procura as variaveis da direita na lista de variaveis e adiciona elas na varsTerms
+                                        if not isTerminal(elemento,terminais): #                                                    (lista de variveis)
+                                            for varGeradora in variaveis:
+                                                    if varGeradora.getValue() == elemento:
+                                                        varsTerms.append(varGeradora)
+
+                                        else: #Se nao estiver na lista de variaveis, é um terminal que vai para a lista varsTerms da variavel à esquerda
+                                            varsTerms.append(elemento)
+                                    variavelAtual.appendTerm(copy.deepcopy(varsTerms))
+
+                else: #Senao é apenas 1 e só instancia objeto que ela é e adiciona à sua lista correspondente
+                    if case == 1:
+                        terminais.append( Terminal( new_phrase[0],None,None ) )
+                    elif case == 2:
+                        new_Variavel = Variable(new_phrase[0],None )
+                        variaveis.append( new_Variavel )
+
+            elif case == 1: #Depois de ler todos os terminais seta que case é 2, ou seja, proximas palavras a lerem serao variaveis
+                case +=1
+
+            elif case == 2: #Depois de ler todos as variaveis seta que case é 3, ou seja, proxima palavra a ser lida sera variavel inicial
+                case +=1
+
+            elif case == 3: #Depois de ler a variavel de inicio seta que case é 4, ou seja, proximas palavras a serem lida serao regras
+                startingWord = new_phrase[0]
+                case +=1
+
+        return variaveis, terminais, startingWord
+
+    except IOError: #Se nao conseguiu se abrir o arquivo
+        print ('Could not open file! Please be sure sample.txt is in the same folder')
+
 
 if __name__ == '__main__':
-    # Ex: Linguagem
-    variables, terminals, firstSymbol = readerFile()
-    firstLooping(firstSymbol, variables)
+    variaveis, terminais, startingVar = readInput()
+    printaVariaveis(variaveis)
